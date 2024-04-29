6008291d442735e4c279b33474dfc611eec1d6e2
Lenovo <msuroor101@gmail.com>
2024-04-29 09:26:31 +0530

    some tweaks:

--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,7 @@ from pet1 import device
 
 # Load the model
 model = QNetwork(state_size=2, action_size=3, seed=0)  # Ensure these parameters match those used during training
-model.load_state_dict(torch.load('dqn_platooning_model.pth'))
+model.load_state_dict(torch.load('./best_dqn_platooning_model.pth'))
 model.to(device)
 model.eval()  # Set the model to evaluation mode
 
@@ -23,8 +23,9 @@ observations = env.reset()
 done = False
 while not done:
     actions = {agent_id: np.argmax(model(torch.from_numpy(observations[agent_id]).float().unsqueeze(0).to(device)).detach().cpu().numpy()) for agent_id in observations}
+    print(actions)
     next_observations, rewards, done, _ = env.step(actions)
-
+    # print(rewards)
     total_reward += sum(rewards.values())
     observations = next_observations
 
